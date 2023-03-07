372e019647c4526ec8f4bac220579a1712e27211
Chaitanya Tejaswi <chaitanya.tejasvi@gmail.com>
2023-03-07 11:30:47 +0530

    Lab Practice Problems

---
+++ pgm41.c
@@ -0,0 +1,25 @@
+// Read the Sentence until '$' appears and count the number of UpperCase, LowerCase, Special case characters and digits
+
+#include <stdio.h>
+int main()
+{
+    char wrd[300];
+    int a[4] = {0, 0, 0, -1};
+    int i = 0;
+    printf("Input: ");
+    do
+    {
+
+        scanf("%c", &wrd[++i]);
+        if (wrd[i] >= 'A' && wrd[i] <= 'Z')
+            a[0]++;
+        else if (wrd[i] >= 'a' && wrd[i] <= 'z')
+            a[1]++;
+        else if (wrd[i] >= '0' && wrd[i] <= '9')
+            a[2]++;
+        else
+            a[3]++;
+    } while (wrd[i] != '\n');
+    printf("\n\nUpperCase Characters: %d\nLowerCase Characters: %d\nDigits: %d\nSpecial Characters: %d\n", a[0], a[1], a[2], a[3]);
+    return 0;
+}
